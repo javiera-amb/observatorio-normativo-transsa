86b75ec48eb2e1ccbf8ef10fe3e51197d3adc6b4
javiera-amb <javiera.morales@transsa.com>
2026-08-06 19:30:23 -0400

    IPT: cargar comparaciones y actos antes de consolidar comunas

--- data/vigencia_cartografica.js
+++ data/vigencia_cartografica.js
@@ -4,4 +4,6 @@ document.write('<script src="data/ipt_vigentes_02.js"><\/script>');
 document.write('<script src="data/ipt_vigentes_03.js"><\/script>');
 document.write('<script src="data/ipt_vigentes_04.js"><\/script>');
 document.write('<script src="data/ipt_vigentes_05.js"><\/script>');
+document.write('<script src="data/comparaciones_ipt.js"><\/script>');
+document.write('<script src="data/actos_ipt.js"><\/script>');
 document.write('<script src="data/vigencia_finalizar.js"><\/script>');
